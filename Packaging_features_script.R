9631b93740bfd4f4d6960a1d0666aa24fcab38d8
Harsh Sahu <harshsahu.8085@gmail.com>
2022-09-02 17:17:23 +0200

    Packaging & rmd

--- Packaging_features_script.R
+++ Packaging_features_script.R
@@ -13,17 +13,21 @@ library(purrr)
 Packaging_data <- read_excel("Packaging.xlsx")
 
 
+#####ANOVA-Modeling#########
+anova(aov(K_value~Film_thickness*Diameter*Air_velocity*Temperature, data=Packaging_data))
+
+
 ######strings#######
 Packaging_data$Film_thickness <- as.factor(Packaging_data$Film_thickness)
 Packaging_data$Diameter <- as.factor(Packaging_data$Diameter)
 Packaging_data$Air_velocity <- as.factor(Packaging_data$Air_velocity)
-Packaging_data$Temperature <- as.numeric(Packaging_data$Temperature)
 
 
-######Graph#########
+######ggplot2_graph#########
 ggplot(Packaging_data, aes(x=Diameter, y=K_value, colour=Temperature, shape=Air_velocity))+        
-  geom_point(size = 3)+
+  geom_point(size = 4)+
   facet_grid(cols = vars(Film_thickness))+
-  theme(axis.text.x = element_text(angle=90))
-
-
+  theme(axis.text.x = element_text(angle=90))+
+  ggtitle("Film_thickness")+
+  scale_color_gradient(low="light green", high="red")+
+  theme(plot.title = element_text(hjust = 0.5))
